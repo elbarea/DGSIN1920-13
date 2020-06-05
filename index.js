ec4447633f396013d7f286629b2f873f662588b9
Enrique Piñero <enrpinfue@alum.us.es>
2020-06-05 16:47:49 +0200

    GET sobre colección

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var dato = { "datasetid": "luftdaten_pm_bristol", "recordid": "15f58e8c75ca64891
 
 const MongoClient = require('mongodb').MongoClient;
 const dburl = "mongodb+srv://admin:dgsin1920@dgsin1920-13-aire-mrw65.mongodb.net/dgsin1920?retryWrites=true&w=majority";
-MongoClient.connect(dburl, (err, client) => {
+var client = MongoClient.connect(dburl, (err, client) => {
 
     if (err) {
         console.error("Error de conexión con la BD" + err);
@@ -32,42 +32,53 @@ const BASE_API = "/api/v1";
 
 
 app.get(BASE_API + "/sensores/loadInitialData", (req, res) => {
-    
-    db.find({}).toArray((err,sensores)=>{
-    if (sensores.length == 0) {
-        console.info("Base de datos vacía, añadiendo valores iniciales");
-        var jsondb = require("./bd/bd.json");
-        for (var i = 0; i < jsondb.length; i++) {
-            var sensorid = jsondb[i].fields.sensor_id;
-            var fecha = jsondb[i].fields.date;
-            var pm10 = jsondb[i].fields.pm10;
-            var pm2_5 = jsondb[i].fields.pm2_5;
-            var latlong = jsondb[i].fields.geo_point_2d;
 
-            var entrada = {
-                "sensorid": sensorid,
-                "fecha": fecha,
-                "pm10": pm10,
-                "pm2_5": pm2_5,
-                "latlong": latlong[0] + ", " + latlong[1]
+    db.find({}).toArray((err, sensores) => {
+        if (sensores.length == 0) {
+            console.info("Base de datos vacía, añadiendo valores iniciales");
+            var jsondb = require("./bd/bd.json");
+            for (var i = 0; i < jsondb.length; i++) {
+                var sensorid = jsondb[i].fields.sensor_id;
+                var fecha = jsondb[i].fields.date;
+                var pm10 = jsondb[i].fields.pm10;
+                var pm2_5 = jsondb[i].fields.pm2_5;
+                var latlong = jsondb[i].fields.geo_point_2d;
+
+                var entrada = {
+                    "sensorid": sensorid,
+                    "fecha": fecha,
+                    "pm10": pm10,
+                    "pm2_5": pm2_5,
+                    "latlong": latlong[0] + ", " + latlong[1]
+                }
+
+                db.insert(entrada);
             }
-
-            db.insert(entrada);
+            console.log("Inserción terminada");
+            res.sendStatus(201);
         }
-        res.sendStatus(201);
-    }
-    else{
-        console.warn("Base de datos no vacía, no se hace nada");
-        res.sendStatus(409);
-    }
+        else {
+            console.warn("Base de datos no vacía, no se hace nada");
+            res.sendStatus(409);
+        }
+    });
 });
 
 
 //Hacer get sobre una colección
 app.get(BASE_API + "/sensores", (req, res) => {
     console.info("Petición GET a /sensores");
+    db.find({}).toArray((err, sensores)=>{
+        if (err) {
+            console.error("Error obteniendo los datos de la BD: " + err);
+            res.sendStatus(500);
+        }
+        else {
+            console.debug("Mandando registros " + JSON.stringify(sensores, null, 2));
+            res.send(sensores);
 
-
+        }
+    });
 
 });
 
@@ -80,4 +91,3 @@ app.listen(process.env.PORT || 8080, () => {
 }).on("error", (e) => {
     console.error("Ha ocurrido un problema");
 });
-
