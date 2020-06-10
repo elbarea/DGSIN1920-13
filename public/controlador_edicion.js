fb86eb99f8af6e97c8e13ce69ba92b3739b90c18
Enrique Piñero <enrpinfue@alum.us.es>
2020-06-10 12:53:12 +0200

    Control de errores/validación de campos introducidos por el cliente

--- public/controlador_edicion.js
+++ public/controlador_edicion.js
@@ -1,27 +1,53 @@
 angular.module("calidadAire")
-    .controller("controlador_edicion", ["$scope", "$http", "$window","$routeParams","$location",function ($scope, $http, $window,$routeParams,$location) {
+    .controller("controlador_edicion", ["$scope", "$http", "$window", "$routeParams", "$location", function ($scope, $http, $window, $routeParams, $location) {
         var id = $routeParams.id;
         var fecha = $routeParams.fecha;
         var base_url = " /api/v1/sensores";
         var url_recurso = base_url + "/" + id + "/" + fecha;
         function listarRecurso() {
-            $http.get(url_recurso).then(function (res) {
-                console.log("Registro recibido: " + JSON.stringify(res.data, null, 2));
-                $scope.Reg = res.data[0];
-            });
+            $http.get(url_recurso).then(function onSuccess(res) {
+                if (res.status == 200 && res.data.length > 0) {
+                    console.log("Registro recibido: " + JSON.stringify(res.data, null, 2));
+                    $scope.Reg = res.data[0];
+                }
+            },
+                function onReject(res) {
+                    if (res.status == 422) {
+                        console.log("Error recuperando registro, formato incorrecto de fecha. Fecha recibida: " + fecha + ", código de estado: " + res.status);
+                        $window.alert("Error al recuperar el registro: formato incorrecto de fecha. Debería ser yyyy-mm-ddThh:mm:ssZ");
+                        $location.path("/list");
+                    }
+                    else if (res.status == 404) {
+                        console.log("Error recuperando registro, recurso no existente. Código de estado: " + res.status);
+                        $window.alert("Error al recuperar el registro: el registro con id " + id + " y fecha " + fecha + " no existe en la base de datos");
+                        $location.path("/list");
+                    }
+                });
         }
 
-        $scope.actualizarRegistro = function actualizarRegistro(){
-            $http.put(url_recurso,$scope.Reg).then(function onSuccess(res){
-                if(res.status == 200){
-                    console.log("Registro actualizado con datos: " + JSON.stringify($scope.Reg,null,2));
+        $scope.actualizarRegistro = function actualizarRegistro() {
+            $http.put(url_recurso, $scope.Reg).then(function onSuccess(res) {
+                if (res.status == 200) {
+                    console.log("Registro actualizado con datos: " + JSON.stringify($scope.Reg, null, 2));
                     $window.alert("Registro actualizado correctamente");
                     $location.path("/list");
                 }
             },
-            function onReject(res){
-                console.log("Registro no actualizado correctamente");
-            });
+                function onReject(res) {
+                    if (res.status == 422) {
+                        console.log("Registro no actualizado correctamente. Puede ser debido a un error de formato de fecha en la URL o en el cuerpo, o debido a que al cuerpo le falte un campo. Código de estado: " + res.status);
+                        $window.alert("No se pudo actualizar el registro. Compruebe el formato de la fecha de la URL y del cuerpo del mensaje, así como que el cuerpo contenga todos los campos necesarios.");
+                    }
+                    else if (res.status == 400) {
+                        console.log("Registro no actualizado correctamente. Petición sin cuerpo. Código de estado: " + res.status);
+                        $window.alert("No se pudo actualizar el registro. La petición no contiene un cuerpo.");
+                    }
+                    else if(res.status == 404){
+                        console.log("Registro no actualizado correctamente: no existe el registro a actualizar. Código de estado: " + res.status);
+                        $window.alert("No se puede actualizar un registro que no existe. Compruebe el id y la fecha del registro.");
+                    }
+
+                });
         }
         listarRecurso();
 
--- public/controlador_listado.js
+++ public/controlador_listado.js
@@ -2,36 +2,57 @@ angular.module("calidadAire")
     .controller("controlador_listado", ["$scope", "$http", "$window", function ($scope, $http, $window) {
         var base_url = " /api/v1/sensores";
         function listarRegistros() {
-            $http.get(base_url).then(function (res) {
-                console.log("Registros recibidos: " + JSON.stringify(res.data, null, 2));
-                $scope.registros = res.data;
-            });
+            $http.get(base_url).then(function onSuccess(res) {
+                if (res.status == 200 && res.data.length > 0) {
+                    console.log("Registros recibidos: " + JSON.stringify(res.data, null, 2));
+                    $scope.registros = res.data;
+                }
+            },
+                function onReject(res) {
+                    console.log("Error recibiendo los datos: " + res.status);
+                    $window.alert("Ha ocurrido un error al recibir los datos. Vuelva a intentarlo de nuevo");
+                });
         }
         $scope.eliminarTodo = function eliminarTodo() {
-            $http.delete(base_url).then((res) => {
+            $http.delete(base_url).then(function onSuccess(res) {
                 if (res.status == 204) {
                     console.log("Registros eliminados");
                     $window.alert("Todos los registros eliminados exitosamente");
                     listarRegistros();
                 }
                 else {
-                    console.log("Error eliminando los registros: " + res.status);
-                    $window.alert("Ha ocurrido un problema eliminando los registros: " + res.status);
                 }
-            });
+            },
+                function onReject(res) {
+                    if (res.status == 404) {
+                        console.log("Error eliminando los registros: no hay registros que eliminar " + res.status);
+                        $window.alert("No se puede eliminar ningún registro ya que no hay ninguno en la base de datos");
+                    }
+                    else {
+                        console.log("Error eliminando los registros: " + res.status);
+                        $window.alert("No se pudo eliminar los registros");
+                    }
+                });
         }
         $scope.eliminarRegistro = function eliminarRegistro(id, fecha) {
-            $http.delete(base_url + "/" + id + "/" + fecha).then((res) => {
+            $http.delete(base_url + "/" + id + "/" + fecha).then(function onSuccess(res) {
                 if (res.status == 204) {
                     console.log("Registro correspondiente al sensor " + id + " con fecha " + fecha + " eliminado");
                     $window.alert("Registro con id " + id + " y fecha " + fecha + " eliminado exitosamente");
                     listarRegistros();
                 }
-                else {
-                    console.log("Error eliminando registro con id " + id + " y fecha " + fecha + " : " + res.status);
-                    $window.alert("Ha ocurrido un problema eliminando los registros: " + res.status);
-                }
-            });
+
+            },
+                function onReject(res) {
+                    if (res.status == 404) {
+                        console.log("Error eliminando registro con id " + id + " y fecha " + fecha + ": no hay registro que eliminar; " + res.status);
+                        $window.alert("Ha ocurrido un problema eliminando los registros: no hay ningún registro con id " + id + " y fecha " + fecha);
+                    }
+                    else if (res.status == 422) {
+                        console.log("Error eliminando registro con id " + id + " y fecha " + fecha + ": formato de fecha incorrecto; " + res.status);
+                        $window.alert("Ha ocurrido un problema eliminando los registros: formato de fecha incorrecto " + fecha);
+                    }
+                });
         }
 
         $scope.crearRegistro = function crearRecurso() {
@@ -57,8 +78,8 @@ angular.module("calidadAire")
 
                     }
                     else if (res.status == 422) {
-                        console.log("Error añadiendo recurso: cuerpo mal formado. Código de estado: " + res.status);
-                        $window.alert("No se pudo añadir el registro debido a que faltan uno o más campos de información");
+                        console.log("Error añadiendo recurso: cuerpo mal formado o formato de fecha incorrecto. Código de estado: " + res.status);
+                        $window.alert("No se pudo añadir el registro debido a que faltan uno o más campos de información o a que la fecha tiene formato incorrecto. El formato debe ser yyyy-mm-ddThh:mm:ssZ");
                     }
                     else {
 
