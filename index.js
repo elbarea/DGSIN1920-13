11e2774b1364d51dd72203037e7cfdab394f98fc
Enrique Piñero <enrpinfue@alum.us.es>
2020-06-11 13:04:35 +0200

    Uso de cabeceras CORS añadido

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var express = require("express")
 var path = require("path");
 var app = express();
-
-var dato = { "datasetid": "luftdaten_pm_bristol", "recordid": "15f58e8c75ca64891c1dc342c8a03115cc862f31", "fields": { "pm2_5": 1.69041666666667, "date": "2019-01-01T03:00:00Z", "sensor_id": "17459", "geo_point_2d": [51.464, -2.58241968111], "pm10": 4.075 }, "geometry": { "type": "Point", "coordinates": [-2.58241968111, 51.464] }, "record_timestamp": "2020-05-08T18:23:22.562+01:00" };
+var cors = require("cors");
 
 const MongoClient = require('mongodb').MongoClient;
 const dburl = "mongodb+srv://admin:dgsin1920@dgsin1920-13-aire-mrw65.mongodb.net/dgsin1920?retryWrites=true&w=majority";
@@ -32,6 +31,7 @@ MongoClient.connect(dburl, (err, client) => {
 
 });
 app.use("/", express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 
 app.listen(process.env.PORT || 8080, () => {
