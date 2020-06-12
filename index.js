facac51528dce89e8d662dea0fb1e6fb18be1a21
Enrique Piñero <enrpinfue@alum.us.es>
2020-06-12 13:31:33 +0200

    Añadido código para mandar peticiones a APIs externas mediante un proxy

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require("express")
 var path = require("path");
 var app = express();
 var cors = require("cors");
+var request = require("request");
+const api_externa = "https://dgsin1920-02.herokuapp.com";
 
 const MongoClient = require('mongodb').MongoClient;
 const dburl = "mongodb+srv://admin:dgsin1920@dgsin1920-13-aire-mrw65.mongodb.net/dgsin1920?retryWrites=true&w=majority";
@@ -31,6 +33,11 @@ MongoClient.connect(dburl, (err, client) => {
 
 });
 app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/proxy13", (req, res) => {
+    var url_api = api_externa + req.url;
+    console.log(req.baseUrl + req.url + " correctamente enrutado");
+    req.pipe(request(url_api)).pipe(res);
+});
 app.use(cors());
 
 
